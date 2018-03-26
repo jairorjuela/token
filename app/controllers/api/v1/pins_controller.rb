c9c35f244a76f6f1c0c1ee41e883f113b0f0f33a
jairorjuela <genius85862@gmail.com>
2018-03-25 19:10:23 -0500

    Token_1

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::PinsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate
+
+
   def index
     render json: Pin.all.order('created_at DESC')
   end
@@ -13,6 +17,28 @@ class Api::V1::PinsController < ApplicationController
   end
 
   private
+
+  def authenticate
+    authenticate_token || render_unauthorized
+  end
+
+  def authenticate_token
+    user = User.find_by(email:request.headers["X-User-Email"])
+    token = request.headers["X-Api-Token"]
+    if (user && token == user.api_token)
+        true
+      else
+        false
+    end
+  end
+
+  def render_unauthorized
+    self.headers["X-Api-Token"] = 'Token realm="Application"'
+    render json: 'Bad credentials', status: 401
+  end
+
+
+
     def pin_params
       params.require(:pin).permit(:title, :image_url)
     end
